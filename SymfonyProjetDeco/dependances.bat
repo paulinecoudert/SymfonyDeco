50fed91be8943974fa03eb75227a12ff0f8e7e6d
coudert <paulinecoudert@gmail.com>
2021-04-15 14:41:05 +0200

    Template et documentation V2

---
+++ SymfonyProjetDeco/dependances.bat
@@ -0,0 +1,7 @@
+composer require annotations
+composer require symfony/orm-pack
+composer require --dev symfony/maker-bundle
+composer require symfony/apache-pack
+composer require symfony/maker-bundle --dev
+composer require --dev doctrine/doctrine-fixtures-bundle
+
