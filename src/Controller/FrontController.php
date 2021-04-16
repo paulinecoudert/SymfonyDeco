cdbd2a0f6d0dec16e95136e0d6fca432a73e95aa
coudert <paulinecoudert@gmail.com>
2021-04-16 17:42:06 +0200

    Template intégrée

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -10,9 +10,33 @@ class FrontController extends AbstractController
 {
 
 
-    #[Route('/index', name: 'index')]
+    #[Route("/index", name: 'index')]
     public function index(): Response
     {
-        return $this->render('front/index.html.twig');
+        return $this->render("front/index.html.twig");
+    }
+
+    #[Route("/projet", name: 'projet')]
+    public function projet(): Response
+    {
+        return $this->render("front/projet.html.twig");
+    }
+
+    #[Route("/contact", name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render("front/contact.html.twig");
+    }
+
+    #[Route("/login", name: 'login')]
+    public function login(): Response
+    {
+        return $this->render("front/login.html.twig");
+    }
+
+    #[Route("/inscription", name: 'inscription')]
+    public function inscription(): Response
+    {
+        return $this->render("front/inscription.html.twig");
     }
 }
